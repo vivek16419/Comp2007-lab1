cf9cb417fa3ac27040d07b53e59896d641156bbe
vivek singh <200361499@student.georgianc.on.ca>
2018-02-13 18:06:42 -0500

    lab

---
+++ Lab1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Products Description.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Products/Details?type=[value}
+        public ActionResult Details(String type)
+        {
+            // check for an empty type
+            if (type== null)
+            {
+                //return View("error");
+                return RedirectToAction("Index");
+            }
+            ViewBag.Type = type;
+            return View();
+        }
+    }
+}
